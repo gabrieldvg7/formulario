0b7f30a36555e205d421d16ef4fd8536f76d0470
Gabriel DVG <gabrieldvg7@gmail.com>
2026-07-27 00:44:14 -0300

    fix: serve frontend from express for production

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
+const path = require('path');
 const express = require('express');
 const cors = require('cors');
 const { port } = require('./src/config/env');
 const leadRoutes = require('./src/routes/leadRoutes');
 
 const app = express();
+const frontendRoot = path.join(__dirname);
 
 app.use(cors({ origin: true }));
 app.use(express.json({ limit: '1mb' }));
@@ -13,6 +15,10 @@ app.get('/health', (_req, res) => {
 });
 
 app.use('/', leadRoutes);
+app.get('/', (_req, res) => {
+  res.sendFile(path.join(frontendRoot, 'index.html'));
+});
+app.use(express.static(frontendRoot));
 
 app.use((err, _req, res, _next) => {
   console.error('[server]', err.message || err);
